5ae5eabae4c6e843fe0636a0398a7b13df34e5ed
xuan son tran <xuanson33bk@gmail.com>
2016-07-27 16:23:01 +0700

    ! init project

---
+++ Assets/Cube.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+	public float moveSpeed = 1f;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Translate (moveSpeed * Input.GetAxis ("Horizontal") * Time.deltaTime,
+		                    0f,
+		                    moveSpeed * Input.GetAxis ("Vertical") * Time.deltaTime);
+	}
+}
